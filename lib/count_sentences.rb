05b588e5529894741082a5128047c765d80a1bb0
Thanh Pham <tpham0123@gmail.com>
2020-05-22 00:30:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
-end
+    #self.split(/[?!.]/).length 
+    sentences = []
+    self.split.each do |word|
+      if word.include?(".")
+        sentences << word
+      elsif 
+        word.include?("?")
+        sentences << word
+      elsif 
+        word.include?("!")
+        sentences << word
+      else 
+      end 
+    end 
+  sentences.length 
+  end 
+end 
+      
+  
